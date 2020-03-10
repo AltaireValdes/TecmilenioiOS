1966c2c0d56dde4f9ccf663a22f4162a4f697fe8
Altaire Valdes <altairevaldes@gmail.com>
2020-03-09 19:02:16 -0600

    Actividad 9 y datos adicionales de pasadas

---
+++ Actividad9/Actividad9/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Actividad9
+//
+//  Created by Altaire Valdes on 27/02/20.
+//  Copyright © 2020 Altaire Valdes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
