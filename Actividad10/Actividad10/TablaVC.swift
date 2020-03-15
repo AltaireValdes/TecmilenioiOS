203ed91403ab04c29f0c5e4c57907cc1589db278
Altaire Valdes <altairevaldes@gmail.com>
2020-03-14 21:23:55 -0600

    Actividad 10 y detalles pasados.

---
+++ Actividad10/Actividad10/TablaVC.swift
@@ -0,0 +1,12 @@
+//
+//  TablaVC.swift
+//  Actividad10
+//
+//  Created by Altaire Valdes on 14/03/20.
+//  Copyright © 2020 Altaire Valdes. All rights reserved.
+//
+
+import Foundation
+
+let nombres = ["Carlos", "Luis", "Pedro", "Jose"]
+
---
+++ Actividad10/Actividad10/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Actividad10
+//
+//  Created by Altaire Valdes on 14/03/20.
+//  Copyright © 2020 Altaire Valdes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
